69c12f589bb288b229677cb318a6b706fd0d7501
George Zhao <38124587+CreatorZZY@users.noreply.github.com>
2026-04-26 22:10:25 +0800

    feat: Implement dynamic background color changes
    Add a setInterval function to change background color based on conditions.

--- src/main.js
+++ src/main.js
@@ -45,4 +45,14 @@
     `;
 
     GM_addStyle(customStyle);
+
+    setInterval(function () {
+        if (document.querySelector("section.el-drawer__body>div>div:nth-child(1)>div>div.explain>div.top>span:nth-child(1)>span") == undefined) {
+            if (document.querySelector("section.el-drawer__body>div>div:nth-child(1)>div>div:nth-child(2)") != undefined)
+                document.querySelector("section.el-drawer__body>div>div:nth-child(1)>div>div:nth-child(2)").style.backgroundColor = "#ffffff"
+        }
+        else if (document.querySelector("section.el-drawer__body>div>div:nth-child(1)>div>div.explain>div.top>span:nth-child(1)>span").innerText === document.querySelector("section.el-drawer__body>div>div:nth-child(1)>div>div.explain>div.top>span:nth-child(2)>span").innerText) { document.querySelector("section.el-drawer__body>div>div:nth-child(1)>div>div:nth-child(2)").style.backgroundColor = "#DAF7DB" }
+        else { document.querySelector("section.el-drawer__body>div>div:nth-child(1)>div>div:nth-child(2)").style.backgroundColor = "#f7dcda" }
+    }, 1000)
+    
 })();
