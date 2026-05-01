5cf11c1c414049e593bd4981dc030b9640399cbf
George Zhao <38124587+CreatorZZY@users.noreply.github.com>
2026-05-01 17:31:44 +0800

    feat: Add input click handling for specific conditions

--- src/main.js
+++ src/main.js
@@ -68,6 +68,16 @@
         else {
             targetEl.style.backgroundColor = "#f7dcda";
         }
+
+        if(
+            document.querySelector("#app > div > div.ele-admin-main > div.ele-admin-body > div.ele-admin-content > div.ele-admin-content-view > div > div.el-card.is-never-shadow > div > div:nth-child(1) > form > div:nth-child(1) > div > div > div > input") != undefined &&
+                document.querySelector("#app > div > div.ele-admin-main > div.ele-admin-body > div.ele-admin-content > div.ele-admin-content-view > div > div.el-card.is-never-shadow > div > div:nth-child(1) > form > div:nth-child(1) > div > div > div > input").value != "快刷模式"
+        ) {
+            if (document.querySelector("body > div.el-select-dropdown.el-popper").getAttribute("x-placement") == undefined)
+                document.querySelector("#app > div > div.ele-admin-main > div.ele-admin-body > div.ele-admin-content > div.ele-admin-content-view > div > div.el-card.is-never-shadow > div > div:nth-child(1) > form > div:nth-child(1) > div > div > div > input").click()
+            else
+                document.querySelector("body > div.el-select-dropdown.el-popper > div.el-scrollbar > div.el-select-dropdown__wrap.el-scrollbar__wrap.el-scrollbar__wrap--hidden-default > ul > li:nth-child(2)").click()
+        }
     }
     const observer2 = new MutationObserver((mutationsList, observer) => {
         checkAnswer();
